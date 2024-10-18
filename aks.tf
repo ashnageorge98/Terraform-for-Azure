2f1e15bbe9afe8deaac65d9e3b44ff0278f56157
Ashna <56075805+ashnageorge98@users.noreply.github.com>
2024-10-18 14:49:55 -0500

    Create aks.tf

---
+++ aks.tf
@@ -0,0 +1,59 @@
+# aks.tf
+
+# Step 1: Define provider
+provider "azurerm" {
+  features {}
+}
+
+# Step 2: Resource group (reuse)
+resource "azurerm_resource_group" "rg" {
+  name     = "myResourceGroup"
+  location = "East US"
+}
+
+# Step 3: Virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myVNet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Step 4: Subnet for AKS
+resource "azurerm_subnet" "aks_subnet" {
+  name                 = "aks-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Step 5: Azure Kubernetes Service (AKS) cluster
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "myAKSCluster"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "myaks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_DS2_v2"
+    vnet_subnet_id = azurerm_subnet.aks_subnet.id
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin = "azure"
+    service_cidr   = "10.0.2.0/24"
+    dns_service_ip = "10.0.2.10"
+    docker_bridge_cidr = "172.17.0.1/16"
+  }
+}
+
+# Output - AKS Cluster Details
+output "aks_cluster_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
