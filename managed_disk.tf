0644a53d15d6b25256c2e7de9a1d6100fc5accdf
Ashna <56075805+ashnageorge98@users.noreply.github.com>
2024-10-22 03:29:43 -0500

    Update managed_disk.tf

--- managed_disk.tf
+++ managed_disk.tf
@@ -1,3 +1,14 @@
+#Managed disk
+resource "azurerm_managed_disk" "example" {
+  name                 = "mymanageddisk"                       # Name of the managed disk
+  resource_group_name  = "myresourcegroup"                     # Resource group name
+  location             = "East US"                             # Location of the disk
+  storage_account_type = "Standard_LRS"                        # Type of storage: Standard or Premium LRS
+  create_option        = "Empty"                               # Option to create an empty disk (others: "FromImage", "Import")
+  disk_size_gb         = 128                                   # Size of the disk in GB
+}
+
+#Unmanaged disk
 resource "azurerm_managed_disk" "example" {
   name                 = "mymanageddisk"                       # Name of the managed disk
   resource_group_name  = "myresourcegroup"                     # Resource group name
