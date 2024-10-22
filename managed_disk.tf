f99b9679abbe18449e77a47fc56f94665dc2e317
Ashna <56075805+ashnageorge98@users.noreply.github.com>
2024-10-22 03:28:20 -0500

    Create managed_disk.tf

---
+++ managed_disk.tf
@@ -0,0 +1,8 @@
+resource "azurerm_managed_disk" "example" {
+  name                 = "mymanageddisk"                       # Name of the managed disk
+  resource_group_name  = "myresourcegroup"                     # Resource group name
+  location             = "East US"                             # Location of the disk
+  storage_account_type = "Standard_LRS"                        # Type of storage: Standard or Premium LRS
+  create_option        = "Empty"                               # Option to create an empty disk (others: "FromImage", "Import")
+  disk_size_gb         = 128                                   # Size of the disk in GB
+}
