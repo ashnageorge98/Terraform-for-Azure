2ece73a615d24c35adf87930f1898a30946d4f77
Ashna <56075805+ashnageorge98@users.noreply.github.com>
2024-10-22 15:17:54 -0500

    Update azureAD.tf

--- azureAD.tf
+++ azureAD.tf
@@ -1,10 +1,12 @@
+# Provider Block: This tells Terraform to use the Azure AD plugin
+
 provider "azuread" {
 tenant_id = "<your-tenant-id>"                         # Azure Ad Tenant ID
 
 # Resource Block: Defines a new Azure AD group
 resource "azuread_group" "example" {
-display_name = "examplegroup"                          #name of AD group
-security_enabled = true                                #Set to true to make it a security group
+display_name = "examplegroup"                          # Name of AD group
+security_enabled = true                                # Set to true to make it a security group
 }
 
 #Resource Block: Defines a new Azure AD user
