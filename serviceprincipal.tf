7849f867bd7a30a522e4ac056b54bdb721dc92b9
Ashna <56075805+ashnageorge98@users.noreply.github.com>
2024-10-22 03:35:52 -0500

    Create serviceprincipal.tf

---
+++ serviceprincipal.tf
@@ -0,0 +1,11 @@
+# Resource Block: Creates an Azure AD service principal from an application
+resource "azuread_service_principal" "example" {
+  application_id = azuread_application.example.application_id  # Links the service principal to the application
+}
+
+# Resource Block: Assigns a role to the service principal
+resource "azurerm_role_assignment" "example" {
+  scope              = "/subscriptions/<subscription-id>"  # Scope for the role assignment
+  role_definition_name = "Reader"  # Role assigned to the service principal, like "Reader"
+  principal_id       = azuread_service_principal.example.id  # Service principal ID
+}
