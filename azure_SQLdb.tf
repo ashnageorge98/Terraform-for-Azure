9e2fd1bb526b2bb1d1bb5722c915c158ab27f4f6
Ashna <56075805+ashnageorge98@users.noreply.github.com>
2024-10-22 17:07:12 -0500

    Create azure_SQLdb.tf

---
+++ azure_SQLdb.tf
@@ -0,0 +1,33 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
+
+resource "azurerm_sql_server" "example" {
+  name                         = "examplensqlserver"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = "sqladmin"
+  administrator_login_password = "P@ssw0rd1234!"  # Use a secure password in real applications
+
+  tags = {
+    environment = "testing"
+  }
+}
+
+resource "azurerm_sql_database" "example" {
+  name                = "exampledb"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  server_name         = azurerm_sql_server.example.name
+  edition             = "Basic"
+
+  tags = {
+    environment = "testing"
+  }
+}
