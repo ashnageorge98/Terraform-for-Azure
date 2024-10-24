cc10e6da6e996117acf442112eb24b20fc0be242
Ashna <56075805+ashnageorge98@users.noreply.github.com>
2024-10-23 20:17:51 -0500

    Update env_dev_variables.tf

--- TFModules/env_dev_variables.tf
+++ TFModules/env_dev_variables.tf
@@ -1,10 +1,16 @@
 variable "resource_group_name" {
-  description = "The name of the resource group."
+  description = "Resource Group name"
   type        = string
 }
 
 variable "location" {
-  description = "The Azure region."
+  description = "Azure region"
   type        = string
   default     = "East US"
+}
+
+variable "sql_admin_password" {
+  description = "The admin password for the SQL Server"
+  type        = string
+  sensitive   = true
 }
