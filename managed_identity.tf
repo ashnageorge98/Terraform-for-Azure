83eb759439bdfe5c906379aca5f7f285d30e5301
Ashna <56075805+ashnageorge98@users.noreply.github.com>
2024-10-31 20:20:21 -0500

    Update managed_identity.tf

--- managed_identity.tf
+++ managed_identity.tf
@@ -1,16 +1,34 @@
 #Managed Identities are special identities in Azure used to give resources like VMs, containers, 
 or other Azure services access to Azure resources without needing to store credentials/secrets.
 
-# Resource Block: Creates a user-assigned managed identity
-resource "azurerm_user_assigned_identity" "example" {
-  name                = "exampleIdentity"       # Name of the managed identity
-  location            = "East US"               # Azure region
-  resource_group_name = "<your-resource-group>"  # Resource group for the managed identity
+# Specify the Azure provider for Terraform
+provider "azurerm" {
+  features {}                         # Enables Azure Resource Manager (azurerm) features. Required for Terraform to use the provider.
+  subscription_id = "81b0b41e-5edd-4af2-86ea-1b1457a4374c"  # Specifies the Azure subscription ID for resource management.
 }
 
-# Resource Block: Assigns a role to the managed identity
-resource "azurerm_role_assignment" "example" {
-  scope              = "/subscriptions/<subscription-id>"  # Scope for the role assignment
-  role_definition_name = "Contributor"  # Role assigned to the managed identity, like "Contributor"
-  principal_id       = azurerm_user_assigned_identity.example.principal_id  # Managed Identity's principal ID
+# Resource group to contain the managed identity resources
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"      # Name of the resource group, which organizes related resources in Azure.
+  location = "East US"                # Specifies the Azure region (location) where the resource group will be created.
 }
+
+# Defines a user-assigned managed identity, allowing Azure resources to access other resources securely
+resource "azurerm_user_assigned_identity" "example_identity" {
+  name                = "example-managed-identity"       # Name for the managed identity, unique within the resource group.
+  location            = azurerm_resource_group.example.location  # Uses the location of the previously defined resource group.
+  resource_group_name = azurerm_resource_group.example.name      # Links the identity to the resource group created above.
+}
+
+# Assigns a specific role to the managed identity, granting permissions within a defined scope
+resource "azurerm_role_assignment" "example_role_assignment" {
+  scope                 = "/subscriptions/81b0b41e-5edd-4af2-86ea-1b1457a4374c"  # Scope for the role assignment; here, the subscription level.
+  role_definition_name  = "Contributor"                                           # Role assigned to the managed identity, e.g., "Contributor" grants create, update, and delete permissions.
+  principal_id          = azurerm_user_assigned_identity.example_identity.principal_id  # Managed identity’s principal ID, used to link the identity with the role assignment.
+}
+
+# Outputs the managed identity ID, which can be used as a reference in other configurations or scripts
+output "managed_identity_id" {
+  value = azurerm_user_assigned_identity.example_identity.id  # Returns the unique ID of the managed identity.
+}
+
