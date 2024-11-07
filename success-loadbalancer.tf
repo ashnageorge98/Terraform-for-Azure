ed6b98cc1f707cb86c3db2884c81e91ed2048fc9
Ashna <56075805+ashnageorge98@users.noreply.github.com>
2024-11-07 15:11:01 -0600

    Update success-loadbalancer.tf

--- success-loadbalancer.tf
+++ success-loadbalancer.tf
@@ -1,27 +1,22 @@
-
 provider "azurerm" {
   features {}
   subscription_id = "81b0b41e-5edd-4af2-86ea-1b1457a4374c"
 }
 
-# Resource group
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "East US"
-}
+# Use existing resource group 'RG1'
 
 # Virtual network
 resource "azurerm_virtual_network" "example_vnet" {
   name                = "exampleVNet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = "East US"  # Set the location for the VNet
+  resource_group_name = "RG1"
 }
 
 # Subnet
 resource "azurerm_subnet" "example_subnet" {
   name                 = "exampleSubnet"
-  resource_group_name  = azurerm_resource_group.example.name
+  resource_group_name  = "RG1"
   virtual_network_name = azurerm_virtual_network.example_vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
@@ -29,8 +24,8 @@ resource "azurerm_subnet" "example_subnet" {
 # Public IP for the Load Balancer
 resource "azurerm_public_ip" "example_pip" {
   name                = "examplePublicIP"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = "East US"  # Set the location for the Public IP
+  resource_group_name = "RG1"
   allocation_method   = "Static"
   sku                 = "Standard"
 }
@@ -38,8 +33,8 @@ resource "azurerm_public_ip" "example_pip" {
 # Load Balancer
 resource "azurerm_lb" "example_lb" {
   name                = "exampleLB"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = "East US"  # Set the location for the Load Balancer
+  resource_group_name = "RG1"
   sku                 = "Standard"
 
   frontend_ip_configuration {
@@ -53,7 +48,6 @@ resource "azurerm_lb_backend_address_pool" "example_bap" {
   name                = "exampleBackendPool"
   loadbalancer_id     = azurerm_lb.example_lb.id
 }
-
 # Health Probe (HTTP)
 resource "azurerm_lb_probe" "example_health_probe" {
   name                = "exampleHealthProbe"
@@ -80,7 +74,7 @@ resource "azurerm_lb_rule" "example_lb_rule" {
 # NAT Rule for SSH access to backend VMs
 resource "azurerm_lb_nat_rule" "example_nat_rule" {
   name                           = "SSHRule"
-  resource_group_name            = azurerm_resource_group.example.name
+  resource_group_name            = "RG1"
   loadbalancer_id                = azurerm_lb.example_lb.id
   protocol                       = "Tcp"
   frontend_port                  = 22
@@ -92,8 +86,8 @@ resource "azurerm_lb_nat_rule" "example_nat_rule" {
 resource "azurerm_network_interface" "example_nic" {
   count               = 2
   name                = "exampleNIC-${count.index}"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = "East US"  # Set location for NIC
+  resource_group_name = "RG1"
 
   ip_configuration {
     name                          = "internal"
@@ -103,23 +97,22 @@ resource "azurerm_network_interface" "example_nic" {
     # Correctly assigning the NIC to the backend address pool
   }
 }
-
 # Virtual Machine
 resource "azurerm_linux_virtual_machine" "example_vm" {
   count               = 2
   name                = "exampleVM-${count.index}"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = "East US"  # Set location for VMs
+  resource_group_name = "RG1"
   size                = "Standard_DS1_v2"
   admin_username      = "azureuser"
   admin_password      = "P@ssw0rd1234!"
   network_interface_ids = [azurerm_network_interface.example_nic[count.index].id]
-  
+
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
-  
+
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
@@ -127,12 +120,3 @@ resource "azurerm_linux_virtual_machine" "example_vm" {
     version   = "latest"
   }
 }
-
-#Notes:
-Load Balancing Rule (Port 80): Distributes HTTP (web) traffic across multiple backend instances.
-Setting backend_port = 80 means that the Load Balancer will forward incoming traffic on frontend_port = 80 to the backend VMs on port 80 as well.
-Purpose: This rule is used for distributing incoming HTTP requests across multiple backend instances (e.g., web servers running on port 80).
-
-NAT Rule (Port 22): Allows SSH access to individual backend VMs, usually for administrative purposes.
-Setting backend_port = 22 means the Load Balancer will forward incoming SSH requests on frontend_port = 22 to backend VMs on port 22 as well.
-Purpose: This rule allows external users to SSH into the backend VMs, typically for administrative access.
