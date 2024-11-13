d0c10762430ee1631e5d82df1d55356bc32d26b3
Ashna <56075805+ashnageorge98@users.noreply.github.com>
2024-11-13 01:19:04 -0600

    Update success-blobstorage.tf

--- success-blobstorage.tf
+++ success-blobstorage.tf
@@ -13,7 +13,7 @@ data "azurerm_resource_group" "existing" {
 
 # Define Storage Account
 resource "azurerm_storage_account" "example" {
-  name                     = "examplestorageacct" # must be globally unique
+  name                     = "uniquename1029" # must be globally unique
   resource_group_name      = data.azurerm_resource_group.existing.name
   location                 = data.azurerm_resource_group.existing.location
   account_tier             = "Standard"
