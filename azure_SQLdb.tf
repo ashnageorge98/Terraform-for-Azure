cf0d5f39a94f99dc1535fa9ee982e9d5f63d2fd3
Ashna <56075805+ashnageorge98@users.noreply.github.com>
2024-11-19 00:55:41 -0600

    Update azure_SQLdb.tf

--- azure_SQLdb.tf
+++ azure_SQLdb.tf
@@ -1,33 +1,87 @@
+# Set the provider
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "East US"
+# Define variables for reusability
+variable "resource_group_name" {
+  default = "my-resource-group"
 }
 
-resource "azurerm_sql_server" "example" {
-  name                         = "examplensqlserver"
-  resource_group_name          = azurerm_resource_group.example.name
-  location                     = azurerm_resource_group.example.location
-  version                      = "12.0"
-  administrator_login          = "sqladmin"
-  administrator_login_password = "P@ssw0rd1234!"                        # Use a secure password in real applications
-
-  tags = {
-    environment = "testing"
-  }
+variable "location" {
+  default = "East US"
 }
 
-resource "azurerm_sql_database" "example" {
-  name                = "exampledb"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  server_name         = azurerm_sql_server.example.name
-  edition             = "Basic"
+variable "sql_server_name" {
+  default = "my-sql-server-123"
+}
 
-  tags = {
-    environment = "testing"
-  }
+variable "sql_database_name" {
+  default = "my-sql-database"
+}
+
+variable "admin_username" {
+  default = "sqladminuser"
+}
+
+variable "admin_password" {
+  default = "P@ssw0rd123!" # Note: Use a secret management tool in real cases
+}
+
+# Resource Group
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# SQL Server
+resource "azurerm_mssql_server" "main" {
+  name                         = var.sql_server_name
+  resource_group_name          = azurerm_resource_group.main.name
+  location                     = azurerm_resource_group.main.location
+  administrator_login          = var.admin_username
+  administrator_login_password = var.admin_password
+  version                      = "12.0" # SQL Server version
+}
+
+# SQL Database
+resource "azurerm_mssql_database" "main" {
+  name                = var.sql_database_name
+  server_id           = azurerm_mssql_server.main.id
+  sku_name            = "S1"         # Pricing tier (Standard S1 as an example)
+  max_size_gb         = 10           # Maximum database size
+  collation           = "SQL_Latin1_General_CP1_CI_AS" # Default collation
+  license_type        = "LicenseIncluded" # Default license type
+  zone_redundant      = false
+  auto_pause_delay_in_minutes = 60  # Use for serverless databases (optional)
+}
+
+# Firewall Rules
+resource "azurerm_sql_firewall_rule" "allow_all_azure_services" {
+  name                = "AllowAzureServices"
+  server_name         = azurerm_mssql_server.main.name
+  resource_group_name = azurerm_resource_group.main.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
+}
+
+resource "azurerm_sql_firewall_rule" "allow_my_ip" {
+  name                = "AllowMyIP"
+  server_name         = azurerm_mssql_server.main.name
+  resource_group_name = azurerm_resource_group.main.name
+  start_ip_address    = "123.123.123.123" # Replace with your IP
+  end_ip_address      = "123.123.123.123"
+}
+
+# Outputs
+output "sql_server_name" {
+  value = azurerm_mssql_server.main.name
+}
+
+output "sql_database_name" {
+  value = azurerm_mssql_database.main.name
+}
+
+output "sql_server_fqdn" {
+  value = azurerm_mssql_server.main.fully_qualified_domain_name
 }
